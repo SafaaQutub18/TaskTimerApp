1ad29f8762709178e7a20cb90c52304a392e8acc
Safaa <safaaqutub@gmail.com>
2022-01-08 02:57:14 +0300

    add chart

--- app/src/main/java/com/example/tasktimerapp/Activity/CategoryActivity.kt
+++ app/src/main/java/com/example/tasktimerapp/Activity/CategoryActivity.kt
@@ -2,8 +2,10 @@ package com.example.tasktimerapp.Activity
 
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.widget.Button
 import android.widget.EditText
@@ -16,8 +18,15 @@ import com.example.tasktimerapp.R
 import com.example.tasktimerapp.TaskViewModel
 import com.example.tasktimerapp.database.Category
 import com.example.tasktimerapp.databinding.ActivityCategoryBinding
-import com.example.tasktimerapp.databinding.ActivityMainBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class CategoryActivity : AppCompatActivity() {
     
@@ -25,6 +34,10 @@ class CategoryActivity : AppCompatActivity() {
     lateinit var iconAdapter : IconAdapter
     lateinit var categoryAdapter : CategoryAdapter
     lateinit var viewModel: TaskViewModel
+
+    private lateinit var barChart: BarChart
+
+    private var categoryList : List<Category> = emptyList()
 
     private val navigasjonen = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -51,10 +64,12 @@ class CategoryActivity : AppCompatActivity() {
         binding = ActivityCategoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.bottomNav.setOnNavigationItemSelectedListener(navigasjonen)
-
         binding.apply {
+        //set bottom nav
+            bottomNav.setOnNavigationItemSelectedListener(navigasjonen)
+
+            //initialize  barChart
+            this@CategoryActivity.barChart= barChar
 
             // icon Adapter setting
             iconAdapter = IconAdapter(this@CategoryActivity) // (this)
@@ -66,14 +81,19 @@ class CategoryActivity : AppCompatActivity() {
             categoryRv.adapter = categoryAdapter
             categoryRv.layoutManager = GridLayoutManager(this@CategoryActivity,2)
 
+
+            drawChar()
         }
 
         viewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
         viewModel.fetchCategories().observe(this, {
             categories -> categoryAdapter.setCategoryList(categories)
+
+            //fill the array of char
+            categoryList = categories
+            drawChar()
         })
     }
-
     fun showAddAlert(image : Int){
 
         val dialog = Dialog(this)
@@ -96,5 +116,69 @@ class CategoryActivity : AppCompatActivity() {
         intent.putExtra("catName", catName)
         startActivity(intent)
     }
+
+    private fun drawChar() {
+        initBarChartUI()
+
+        //now draw bar chart with dynamic data
+        val entries: ArrayList<BarEntry> = ArrayList()
+
+        //you can replace this data object with  your custom object
+        for (i in categoryList.indices) {
+            val categ = categoryList[i]
+            //entries.add(BarEntry(i.toFloat(), categ.categoryTime.toFloat()))
+            entries.add(BarEntry(i.toFloat(), 4.toFloat()))
+        }
+
+
+        val barDataSet = BarDataSet(entries, "Category")
+        barDataSet.setColors(Color.parseColor("#BAB0F7"),Color.parseColor("#95DEFF"),Color.parseColor("#FFBED6"), Color.parseColor("#E4A9FC"),Color.parseColor("#42CFF9"),)
+        val data = BarData(barDataSet)
+        barChart.data = data
+
+        barChart.invalidate()
+    }
+
+    private fun initBarChartUI() {
+        //hide grid lines
+        barChart.axisLeft.setDrawGridLines(false)
+        val xAxis: XAxis = barChart.xAxis
+        xAxis.setDrawGridLines(true)
+        xAxis.setDrawAxisLine(true)
+
+        //remove right y-axis
+        barChart.axisRight.isEnabled = false
+
+        //remove legend
+        barChart.legend.isEnabled = false
+
+        //remove description label
+        barChart.description.isEnabled = false
+
+        //add animation
+        barChart.animateY(2000)
+
+        // to draw label on xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM_INSIDE
+        xAxis.valueFormatter = MyAxisFormatter()
+        xAxis.setDrawLabels(true)
+        xAxis.granularity = 1f
+        xAxis.labelRotationAngle = +90f
+    }
+
+    inner class MyAxisFormatter : IndexAxisValueFormatter() {
+
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            val index = value.toInt()
+            Log.d("Main Activity", "getAxisLabel: index $index")
+            return if (index < categoryList.size) {
+                categoryList[index].categoryName
+            } else {
+                ""
+            }
+        }
+    }
+
+
 
 }
